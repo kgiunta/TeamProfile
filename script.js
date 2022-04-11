26c3ea54c4a4456b5293639ffa0c16216966f130
Kgiunta <giunta.kyle@yahoo.com>
2022-04-11 15:02:58 -0400

    restart questions

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const Employee = require("./lib/Employee");
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
+let allEmployees = [];
 
 // total random puesdo for constructors
 // function userInputs(name, role, id, email, github, school)
@@ -22,84 +23,142 @@ const Intern = require("./lib/Intern");
 // const engineer= new userInputs(name,role,id,email,github,)
 // const intern= new userInputs(name,role,id,email,github,school,)
 
-inquirer
-  .prompt([
-    {
-      type: "list",
-      name: "role",
-      message: "Please select the role you would like to fill out: ",
-      choices: ["Manager", "Engineer", "Intern"],
-    },
+// put prompt into a function to call back to run through questions again call at bottom
+function createCards() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "role",
+        message: "Please select the role you would like to fill out: ",
+        choices: ["Manager", "Engineer", "Intern"],
+      },
 
-    {
-      type: "input",
-      name: "name",
-      message: "What is your name?",
-    },
-    {
-      type: "input",
-      message: "What is your ID number?",
-      name: "idNum",
-    },
-    {
-      type: "input",
-      message: "What is your email?",
-      name: "email",
-    },
+      {
+        type: "input",
+        name: "name",
+        message: "What is your name?",
+      },
+      {
+        type: "input",
+        message: "What is your ID number?",
+        name: "idNum",
+      },
+      {
+        type: "input",
+        message: "What is your email?",
+        name: "email",
+      },
 
-    {
-      type: "input",
-      message: "Please enter office number",
-      name: "number",
-      when: (response) => {
-        if (response.role === "Manager") return true;
+      {
+        type: "input",
+        message: "Please enter office number",
+        name: "number",
+        when: (response) => {
+          if (response.role === "Manager") return true;
+        },
       },
-    },
-    {
-      type: "input",
-      message: "Please enter your GitHub",
-      name: "github",
-      when: (response) => {
-        if (response.role === "Engineer") return true;
+      {
+        type: "input",
+        message: "Please enter your GitHub",
+        name: "github",
+        when: (response) => {
+          if (response.role === "Engineer") return true;
+        },
       },
-    },
-    {
-      type: "input",
-      message: "What school do you attend?",
-      name: "school",
-      when: (response) => {
-        if (response.role === "Intern") return true;
+      {
+        type: "input",
+        message: "What school do you attend?",
+        name: "school",
+        when: (response) => {
+          if (response.role === "Intern") return true;
+        },
       },
-    },
-  ])
-  .then((data) => {
-    const filename = createCard(data);
-    fs.appendFile("index.html", filename, (err) => {
-      err ? console.log(err) : console.log("Success!");
+      {
+        type: "list",
+        message: "Would you like to add another employee?",
+        name: "option",
+        choices: ["Yes", "No"],
+      },
+    ])
+
+    .then((data) => {
+      if (data.option === "Yes") {
+        allEmployees.push(data);
+        return createCards();
+      } else {
+        const filename = createCard(data);
+        fs.appendFile("index.html", filename, (err) => {
+          err ? console.log(err) : console.log("Success!");
+        });
+      }
     });
-  });
-
-function createCard(data) {
-  return `
+  // push data to buildTeam
+  function createCard(data) {
+    if ((data.role = "Manager")) {
+      return `
             <div class="card row" style="width: 18rem">
               <div class="card-body">
                 <h5 class="card-title">${data.name}</h5>
                 <p class="card-text">
-                  Some quick example text to build on the card title and make up the
-                  bulk of the card's content.
+                  ${data.role}
                 </p>
               </div>
               <ul class="list-group list-group-flush">
                 <li class="list-group-item">ID: ${data.idNum}</li>
-                <li class="list-group-item">Phone: ${data.email}</li>
-                <li class="list-group-item">Vestibulum at eros</li>
+                <li class="list-group-item">Email: <a href ="mailto:${data.email}></a></li>
+                <li class="list-group-item">Phone:${data.number}</li>
               </ul>
               <div class="card-body">
-                <a href="#" class="card-link">Card link</a>
-                <a href="#" class="card-link">Another link</a>
+               
               </div>
             </div>
           </div>
        
-      `;
+     ;`;
+    }
+
+    if ((data.role = "Engineer")) {
+      return `<div class="card row" style="width: 18rem">
+               <div class="card-body">
+                 <h5 class="card-title">${data.name}</h5>
+                 <p class="card-text">
+                  ${data.role}
+                 </p>
+               </div>
+               <ul class="list-group list-group-flush">
+                 <li class="list-group-item">ID: ${data.idNum}</li>
+                 <li class="list-group-item">Phone: ${data.email}</li>
+               </ul>
+               <div class="card-body">
+                 <a href="https://github.com/${data.github}"target=""_blank"> class="card-link">Github</a>
+
+               </div>
+             </div>
+           </div>
+
+       `;
+    }
+    if ((data.role = "Intern")) {
+      return `
+               <div class="card row" style="width: 18rem">
+                 <div class="card-body">
+                    <h5 class="card-title">${data.name}</h5>
+                    <p ${data.role}
+                     </p>
+                   </div>
+                   <ul class="list-group list-group-flush">
+                     <li class="list-group-item">ID: ${data.idNum}</li>
+                     <li class="list-group-item">Phone: ${data.email}</li>
+                     <li class="list-group-item">School: ${data.school}</li>
+                   </ul>
+                   <div class="card-body">
+                   </div>
+                 </div>
+               </div>
+    
+           `;
+    }
+  }
 }
+createCards();
